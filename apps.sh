c82966fb0a2921562b4c5d9f75110f65d84eb6c6
gabs <110247388+GabsEdits@users.noreply.github.com>
2023-01-22 18:43:46 +0200

    Create apps.sh

---
+++ apps.sh
@@ -0,0 +1,8 @@
+# Install all the apps
+flatpak install flathub org.mozilla.firefox
+flatpak install flathub com.viber.Viber
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub com.spotify.Client
+flatpak install flathub com.visualstudio.code-oss
+
+# That's it bc VanillaOS is super optimized out of the box
