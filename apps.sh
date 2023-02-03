ddacc3af8db8544e017ca20321150b7bc41c02a3
gabs <110247388+GabsEdits@users.noreply.github.com>
2023-02-03 18:43:45 +0000

    Add hostname change

--- apps.sh
+++ apps.sh
@@ -1,3 +1,6 @@
+if [[ $EUID -ne 0 ]]; then
+  echo "Hi, Welcome to my Vanilla install we are going to get started in any moment..." 2>&1
+fi
 # Install all the apps
 flatpak install flathub org.mozilla.firefox
 flatpak install flathub com.viber.Viber
@@ -5,4 +8,6 @@ flatpak install flathub com.discordapp.Discord
 flatpak install flathub com.spotify.Client
 flatpak install flathub com.visualstudio.code-oss
 
+# Change hostname
+hostnamectl set-hostname "dell"
 # That's it bc VanillaOS is super optimized out of the box
